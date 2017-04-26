20626b19a864f23e558223236a1afe7f5722fb17
William Fulton <wyfulton@gmail.com>
2017-04-25 21:51:53 -0600

    add playground for mongoose-queries

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,45 @@
+const {ObjectID} = require('mongodb')
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo')
+const {User} = require('./../server/models/user');
+
+var id = '58ecf7fac85377ab1b3ffcb';
+
+// if(!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos: ', todos);
+// });
+//
+// Todo.findOne({_id:id}).then((todos) => {
+//   console.log("Todos: ", todos);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if(!todo){
+//     return console.log('Id not found');
+//   }
+//   console.log(todo)
+// }).catch((err) => {
+//   console.log(err);
+// });
+
+var userId = '58e5b0d0018e489a45523bdf';
+
+if (!ObjectID.isValid(userId)) {
+  console.log('user id not valid');
+}
+
+User.findById(userId).then((user) => {
+  if(!user){
+    return console.log('User not found');
+  }
+  console.log('User: ', user);
+}).catch((e) => {
+  console.log(e);
+});
